3b1080157177452efa8f0cc0ae2caf8f934b95b3
raymondd71 <raymondd71@nycstudents.net>
2024-10-31 12:26:34 -0400

    ddsadsadsadsa

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,3 +1,12 @@
 import "../CSS/style.css";
 import { valorantAgents } from "./agent";
-valorantAgents.forEach((agent) => console.log(agent));
+
+valorantAgents.forEach((agent) =>
+  document.querySelector(".container").insertAdjacentHTML(
+    "beforeend",
+    `<div class= "card"> 
+    <h1>${agent["name"]}<h1> 
+      <h4> Class: ${agent["class"]}</h4>
+      </div>`
+  )
+);
